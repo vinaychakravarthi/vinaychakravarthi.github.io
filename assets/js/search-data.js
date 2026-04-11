35877c82c37d73c91daa698db6a6623964c0f4c1
vinaychakravarthi <36322305+vinaychakravarthi@users.noreply.github.com>
2026-04-11 21:23:50 +0000

    Deploying to gh-pages from @ vinaychakravarthi/vinaychakravarthi.github.io@18842b9f3e5d2fec397ecc364e837237ad2f7765 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-carlsberg-foundation-granted-80000-dkk-to-support-scandinavian-conference-on-ai-scai-2026",
+          title: 'Carlsberg Foundation granted 80000 DKK to support Scandinavian conference on AI (SCAI), 2026....',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
