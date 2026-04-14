0da02c63c02efaa74aeb66cc1ca996c9ee83ac29
vinaychakravarthi <36322305+vinaychakravarthi@users.noreply.github.com>
2026-04-14 11:12:12 +0000

    Deploying to gh-pages from @ vinaychakravarthi/vinaychakravarthi.github.io@21c615712afeb0a38ff86625a6c5218240ae23ee 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,13 +44,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-teaching",
-          title: "Teaching",
-          description: "Course materials, schedules, and resources for classes taught.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/Teaching/";
-          },
         },{id: "nav-people",
           title: "people",
           description: "members of the lab or group",
