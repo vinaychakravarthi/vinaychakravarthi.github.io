51a71d481e8961d2b55925a597125c2471e567af
vinaychakravarthi <36322305+vinaychakravarthi@users.noreply.github.com>
2026-04-14 09:57:07 +0000

    Deploying to gh-pages from @ vinaychakravarthi/vinaychakravarthi.github.io@ed0347084a97bf8c20243d5fc11df91253d6248d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -499,7 +499,7 @@ ninja.data = [{
               window.location.href = "/projects/9_project/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
-          description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
+          description: "",
           section: "Teachings",handler: () => {
               window.location.href = "/teachings/data-science-fundamentals/";
             },},{id: "teachings-introduction-to-machine-learning",
