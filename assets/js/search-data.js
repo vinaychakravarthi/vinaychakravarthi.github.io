2a906ae1429e8799050d9acdff368b9accd8ca81
vinaychakravarthi <36322305+vinaychakravarthi@users.noreply.github.com>
2026-09-25 22:14:16 +0000

    Deploying to gh-pages from @ vinaychakravarthi/vinaychakravarthi.github.io@ab6edf7a95e59feaa304bcbeb22ecc20c40501be 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -416,17 +416,7 @@ ninja.data = [{
           section: "News",},{id: "news-carlsberg-foundation-granted-80000-dkk-to-support-scandinavian-conference-on-ai-scai-2026-i-sincerely-thank-the-carlsberg-foundation-for-their-generous-support",
           title: 'Carlsberg Foundation granted 80000 DKK to support Scandinavian Conference on AI (SCAI), 2026!...',
           description: "",
-          section: "News",},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
+          section: "News",},{id: "projects-project-9",
           title: 'project 9',
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
