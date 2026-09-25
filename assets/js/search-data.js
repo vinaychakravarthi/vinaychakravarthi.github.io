23f590aa981f82b81fc6b6c3c4e2fc52c3375eba
vinaychakravarthi <36322305+vinaychakravarthi@users.noreply.github.com>
2026-09-25 22:08:24 +0000

    Deploying to gh-pages from @ vinaychakravarthi/vinaychakravarthi.github.io@8d911c2b53c768fe1ae87cdfc7626b506243fbd0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "Projects",
-          description: "A growing collection of your cool projects.",
+          description: "Research projects I lead or take part in.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/Projects/";
